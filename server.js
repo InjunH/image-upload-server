a4d706bd061d8418328cb4157acf9ccc2253b492
INJUN HWANG <ijhwang@pood.pet>
2022-03-14 00:54:43 +0900

    uuid로 이미지 고유 아이디 생성 해주기

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const multer = require('multer');
+const {v4: uuid} = require('uuid');
+const mime = require('mime-types');
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => cb(null, "./uploads"),
-    filename: (req, file, cb) => cb(null, file.originalname)
+    filename: (req, file, cb) => cb(null, `${uuid()}.${mime.extension(file.mimetype)}`)
   })
 // multer.diskStorage를 이용하면 파일 저장과정을 제어할 수 있음
-
+const upload = multer( {storage} );
 const app = express();
 const PORT = 5001;
 
@@ -14,7 +17,7 @@ const PORT = 5001;
 upload.single("imageTest") 라는 미들웨어 추가해줌으로서
 이미지 받을 수 있다
 */
-app.post("/upload", upload.single("imageTest") , (req, res) => {
+app.post("/upload",upload.single("imageTest") , (req, res) => {
 
     console.log(req.file);
     res.json(req.file);
