64268c7c7ad7f46bc95ab57094eb2a4b0ee6a748
INJUN HWANG <ijhwang@pood.pet>
2022-03-14 00:04:14 +0900

    multer.diskStorage로 filename 수정하기

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads'});
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, "./uploads"),
+    filename: (req, file, cb) => cb(null, file.originalname)
+  })
+// multer.diskStorage를 이용하면 파일 저장과정을 제어할 수 있음
 
 const app = express();
 const PORT = 5001;
