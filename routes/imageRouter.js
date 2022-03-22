808b15201caeb1117b1462397b91f0e644c316a0
INJUN HWANG <ijhwang@pood.pet>
2022-03-22 21:51:53 +0900

    app.js 에서 /image 선언하여 제거

--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -7,12 +7,12 @@ const { upload } = require("../middleware/imageUpload");
 upload.single("imageTest") 라는 미들웨어 추가해줌으로서
 이미지 받을 수 있다
 */
-imageRouter.post("/images",upload.single("images") , async (req, res) => {
+imageRouter.post("/",upload.single("images") , async (req, res) => {
     const image = await new Image({key: req.file.filename, originalFileName: req.file.originalname}).save()
     res.json(image);
 });
 
-imageRouter.get("/images", async (req , res) => {
+imageRouter.get("/", async (req , res) => {
     const images = await Image.find();
     res.json(images); 
 })
