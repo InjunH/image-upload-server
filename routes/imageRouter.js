9e48f1fa5bb1996e07f00b0e7a1f606c6536004b
INJUN <ijhwang@pood.pet>
2022-05-25 01:18:06 +0900

    image 관련 사전 작업

--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -2,16 +2,18 @@ const { Router } = require("express");
 const imageRouter = Router();
 const Image = require("../models/Image");
 const { upload } = require("../middleware/imageUpload");
+const fs = require("fs");
+const { promisify } = require("util");
+const mongoose = require("mongoose");
+const { v4: uuid } = require("uuid");
+const mime = require("mime-types");
 
-/*
-upload.single("imageTest") 라는 미들웨어 추가해줌으로서
-이미지 받을 수 있다
-*/
+imageRouter.post("presigned", async (req, res) => {
+  try {
+  } catch (error) {}
+});
+
 imageRouter.post("/", upload.single("images"), async (req, res) => {
-  // 유저 정보 확인 , public 권한 확인
-
-  //   console.log(req.file);
-
   const image = await new Image({
     key: req.file.filename,
     originalFileName: req.file.originalname,
@@ -20,23 +22,98 @@ imageRouter.post("/", upload.single("images"), async (req, res) => {
 });
 
 imageRouter.get("/", async (req, res) => {
-  // public 이미지만 제공
-  const images = await Image.find();
-  res.json(images);
+  try {
+    const { lastid } = req.query;
+    if (lastid && !mongoose.isValidObjectId(lastid))
+      throw new Error("invalid lastid");
+    const images = await Image.find(
+      lastid
+        ? {
+            public: true,
+            _id: { $lt: lastid },
+          }
+        : { public: true }
+    )
+      .sort({ _id: -1 })
+      .limit(30);
+    res.json(images);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: err.message });
+  }
 });
 
-// imagetRouter.delete("/:imageId", (req, res) => {
-//   //권한 확인
-//   // 사진 삭제
-// });
+imageRouter.get("/:imageId", async (req, res) => {
+  try {
+    const { imageId } = req.params;
+    if (!mongoose.isValidObjectId(imageId))
+      throw new Error("올바르지 않는 이미지id입니다.");
+    const image = await Image.findOne({ _id: imageId });
+    if (!image) throw new Error("해당 이미지는 존재 하지 않습니다.");
+    if (!image.public && (!req.user || req.user.id !== image.user.id))
+      throw new Error("권한이 없습니다.");
+    res.json(image);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: err.message });
+  }
+});
 
-// imageRouter.patch("/:imageId/like", (req, res) => {
-//   // 유저 권한 확인
-//   //like 중복안되도록
-// });
-// imageRouter.patch("/imageId/unlike", (req, res) => {
-//   // 유저 권한 확인
-//   //like 중복 취소 안되도록
-// });
+imageRouter.delete("/:imageId", async (req, res) => {
+  try {
+    if (!req.user) throw new Error("권한이 없습니다.");
+    if (!mongoose.isValidObjectId(req.params.imageId))
+      throw new Error("올바르지 않은 이미지id입니다.");
+
+    const image = await Image.findOneAndDelete({ _id: req.params.imageId });
+    if (!image)
+      return res.json({ message: "요청하신 사진은 이미 삭제되었습니다." });
+    // await fileUnlink(`./uploads/${image.key}`);
+    s3.deleteObject(
+      { Bucket: "image-upload-tutorial", Key: `raw/${image.key}` },
+      (error) => {
+        if (error) throw error;
+      }
+    );
+    res.json({ message: "요청하신 이미지가 삭제되었습니다.", image });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: err.message });
+  }
+});
+
+imageRouter.patch("/:imageId/like", async (req, res) => {
+  try {
+    if (!req.user) throw new Error("권한이 없습니다.");
+    if (!mongoose.isValidObjectId(req.params.imageId))
+      throw new Error("올바르지 않은 imageId입니다.");
+    const image = await Image.findOneAndUpdate(
+      { _id: req.params.imageId },
+      { $addToSet: { likes: req.user.id } },
+      { new: true }
+    );
+    res.json(image);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: err.message });
+  }
+});
+
+imageRouter.patch("/:imageId/unlike", async (req, res) => {
+  try {
+    if (!req.user) throw new Error("권한이 없습니다.");
+    if (!mongoose.isValidObjectId(req.params.imageId))
+      throw new Error("올바르지 않은 imageId입니다.");
+    const image = await Image.findOneAndUpdate(
+      { _id: req.params.imageId },
+      { $pull: { likes: req.user.id } },
+      { new: true }
+    );
+    res.json(image);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: err.message });
+  }
+});
 
 module.exports = { imageRouter };
